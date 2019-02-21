e1215ff2c807db2ed392f741810c88180a5cbb2b
Jefferson Viana <jeffvfa@hotmail.com>
2019-02-21 12:37:36 -0300

    matrix being created

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-//is pair
-var num1 = 35;
+const width = 2;
+const heigth = 3;
+const matrix = [];
 
-function isPair(num) {
-    if (num%2 !== 0) {
-        alert('is not pair');
-        return 0;
-    }
-    else {
-        alert('is pair');
-        return 1;
+function start(){
+    alert('test');
+    render();
+}
+
+
+function createMatrix() {
+    const matrix_size = width * heigth;
+
+    for (var i = 0; i < matrix_size; i++) {
+        matrix[i] = 0;
     }
 }
 
-var num2 = prompt('colocar o número')
-isPair(num2)
+function animateFire() {
+
+}
+
+function render() {
+    let html = "<table> ";
+    for (var row = 0; row < width; row++) {
+        html += "<tr>";
+        for (var column = 0; column < heigth; column++) {
+            pixel_index = column + (row*width);
+
+            html += "<td>";
+            html += pixel_index;
+            html += "</td>";
+        }
+        html += "</tr>";
+    }
+    html += "</table>";
+    document.querySelector('#fire').innerHTML = html;
+}
+
+
+start();
