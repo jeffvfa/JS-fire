e28513739c87dc3cafc6051f090f637d21b99c66
Jefferson Viana <jeffvfa@hotmail.com>
2019-02-22 10:16:58 -0300

    implementation of animateFire

--- script.js
+++ script.js
@@ -1,33 +1,46 @@
 const width = 4;
-const heigth = 8;
+const height = 10;
 const matrix = [];
+const matrix_size = width * height;
 
 function start(){
+    createMatrix()
     render();
+    animateFire();
 }
 
 
 function createMatrix() {
-    const matrix_size = width * heigth;
+
 
     for (var i = 0; i < matrix_size; i++) {
         matrix[i] = 0;
     }
+    initialize_fire();
 }
 
 function animateFire() {
+    while(1){
+        for (var i = 0; i < matrix_size - width; i++) {
+            let new_value = (matrix[i + width] -1)
+            matrix[i] = (new_value<0)? 0 : new_value;
 
+        }
+        render()
+    }
 }
 
 function render() {
     let html = "<table> ";
-    for (var row = 0; row < heigth; row++) {
+    for (var row = 0; row < height; row++) {
         html += "<tr>";
         for (var column = 0; column < width; column++) {
-            pixel_index = column + (row*width);
+            const pixel_index = column + (row*width);
+            const fire_intensity = matrix[pixel_index];
 
             html += "<td>";
-            html += pixel_index;
+            html += "<div class='pixel_index'>" + pixel_index + " </div>";
+            html += fire_intensity
             html += "</td>";
         }
         html += "</tr>";
@@ -35,6 +48,16 @@ function render() {
     html += "</table>";
     document.querySelector('#fire').innerHTML = html;
 }
+
+function initialize_fire(){
+    index = matrix_size -1;
+
+    for (var i = 0; i < width; i++) {
+        matrix[index] = 36;
+        index--;
+    }
+}
+
 
 
 start();
